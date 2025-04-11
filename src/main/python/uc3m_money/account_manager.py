6b76f061ea58f00264dc918607826ee4c8018609
100522245 <100522245@alumnos.uc3m.es>
2025-04-11 11:23:51 +0200

    Refactorizamos lo siguiente: 1. Borramos la asignación innecesaria: iban = iban 2. El atributo original_code pasa a ser digitos_control 3. El atributo iban, debajo de este, pasa a ser parte_numerica_iban 4. El atributo iban, debajo de este, pasa a ser iban_reordenado 5. Reagrupamos la parte de código que hace replace a las letras 6. El atributo int_i pasa a ser iban_integer 7. El atributo mod pasa a ser mod_resultado 8. El atributo dc pasa a ser digitos_control_esperados Respecto a este último simplificamos un poco el código.

--- src/main/python/uc3m_money/account_manager.py
+++ src/main/python/uc3m_money/account_manager.py
@@ -31,40 +31,37 @@ class AccountManager:
         formato_iban = re.compile(r"^ES[0-9]{22}")
         if not formato_iban.fullmatch(iban):
             raise AccountManagementException("Invalid IBAN format")
-        iban = iban
-        original_code = iban[2:4]
+
+        digitos_control = iban[2:4]
         #replacing the control
-        iban = iban[:2] + "00" + iban[4:]
-        iban = iban[4:] + iban[:4]
+        parte_numerica_iban = iban[:2] + "00" + iban[4:]
+        iban_reordenado = parte_numerica_iban[4:] + parte_numerica_iban[:4]
 
 
         # Convertir el IBAN en una cadena numérica, reemplazando letras por números
-        iban = (iban.replace('A', '10').replace('B', '11').
+        parte_numerica_iban = (iban_reordenado.replace('A', '10').replace('B', '11').
                 replace('C', '12').replace('D', '13').replace('E', '14').
-                replace('F', '15'))
-        iban = (iban.replace('G', '16').replace('H', '17').
+                replace('F', '15').replace('G', '16').replace('H', '17').
                 replace('I', '18').replace('J', '19').replace('K', '20').
-                replace('L', '21'))
-        iban = (iban.replace('M', '22').replace('N', '23').
+                replace('L', '21').replace('M', '22').replace('N', '23').
                 replace('O', '24').replace('P', '25').replace('Q', '26').
-                replace('R', '27'))
-        iban = (iban.replace('S', '28').replace('T', '29').replace('U', '30').
-                replace('V', '31').replace('W', '32').replace('X', '33'))
-        iban = iban.replace('Y', '34').replace('Z', '35')
+                replace('R', '27').replace('S', '28').replace('T', '29').replace('U', '30').
+                replace('V', '31').replace('W', '32').replace('X',
+                                                              '33').replace('Y', '34').replace('Z', '35'))
 
         # Mover los cuatro primeros caracteres al final
 
         # Convertir la cadena en un número entero
-        int_i = int(iban)
+        iban_integer = int(iban)
 
         # Calcular el módulo 97
-        mod = int_i % 97
+        mod_resultado = iban_integer % 97
 
         # Calcular el dígito de control (97 menos el módulo)
-        dc = 98 - mod
+        digitos_control_esperados = 98 - mod_resultado
 
-        if int(original_code) != dc:
-            #print(dc)
+        if int(digitos_control) != digitos_control_esperados:
+            #print(digitos_control_esperados)
             raise AccountManagementException("Invalid IBAN control digit")
 
         return iban
